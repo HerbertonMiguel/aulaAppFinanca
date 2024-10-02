0673089bef8637bb577e3e408ae4e4631323c934
Herberton Miguel <herbertonmiguel@gmail.com>
2024-10-02 23:14:00 +0000

    feito pagina cadastro

--- aulaAppFinanca/src/pages/SignUp/index.js
+++ aulaAppFinanca/src/pages/SignUp/index.js
@@ -1,10 +1,36 @@
 import React from 'react'
 import  {View, Text} from 'react-native'
 
+import {Background, Container, AreaInput, Input, SubmitButton, SubmitText} from '../SignIn/styles'
+
 export default function SignUp(){
     return (
-       <View>
-        <Text> TELA CADASTRO</Text>
-       </View> 
+       <Background>
+        <Container>
+            <AreaInput>
+                    <Input
+                        placeholder="Nome"
+                    />
+            </AreaInput>
+
+            <AreaInput>
+                    <Input
+                        placeholder="Seu email"
+                    />
+            </AreaInput>
+
+            <AreaInput>
+                    <Input
+                        placeholder="Sua Senha"
+                    />
+            </AreaInput>
+
+            <SubmitButton activeOpacity={0.8} >
+                <SubmitText>Cadastrar</SubmitText>
+            </SubmitButton>
+
+        </Container>
+       </Background>
+       
     )
 }
